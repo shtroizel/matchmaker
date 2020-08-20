f8a7ebb03c561896d1aced19c9b919c8f2606cc2
shtroizel <51969765+shtroizel@users.noreply.github.com>
2020-08-20 07:48:14 +0200

    initial commit for stage 1 data reader
      * stage 1 data reader links to a built (stage 0) matchmaker
        library and uses matchmaker for the processing of yet more
        data - specifically synonyms and antonyms (for now)
      * 51155 implemented for initial commit, 3202 to follow later
      * at this point matchmaker is still single-stage - the new
        data reader will be first used in a later commit

---
+++ data_reader/stage_1/scripts/build_and_install.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python3
+
+import os
+import multiprocessing
+import shutil
+import subprocess
+import sys
+import getopt
+from shutil import which
+
+
+def usage():
+    print('options for building data_reader_stage_1:\n')
+    print('    -h, --help            print this message\n')
+    print('    -b  --build_dir       build directory')
+    print('                            * defaults to <data_reader_stage_1 root>/build')
+    print('                            * relative paths are relative to <data_reader_stage_1 root>\n')
+    print('    -i  --install_dir     install directory')
+    print('                            * defaults to <data_reader_stage_1 root>/install')
+    print('                            * relative paths are relative to build_dir\n')
+    print('    -m  --matchable_dir   matchable install directory')
+    print('                            * defaults to <data_reader_stage_1 root>/../matchable/install\n')
+    print('    -s  --stage_0_dir     matchmaker stage 0 install directory\n')
+    print('    -c  --clang           force use of clang compiler')
+    print('                            * system compiler used by default\n')
+    print('    -d  --debug           debug build')
+    print('                            * default is release\n')
+
+
+
+def build_and_install(build_dir, install_dir, matchable_dir, stage_0_dir, use_clang, debug):
+    start_dir = os.getcwd()
+
+    data_reader_stage_1_root = os.path.dirname(os.path.realpath(__file__)) + '/../'
+    os.chdir(data_reader_stage_1_root)
+
+    if build_dir == '':
+        build_dir = data_reader_stage_1_root + '/build'
+    while build_dir[-1] == '/':
+        build_dir = build_dir[:-1]
+
+    if install_dir == '':
+        install_dir = data_reader_stage_1_root + '/install'
+    while install_dir[-1] == '/':
+        install_dir = install_dir[:-1]
+
+    if matchable_dir == '':
+        matchable_dir = data_reader_stage_1_root + '/../../matchable/install'
+    while matchable_dir[-1] == '/':
+        matchable_dir = matchable_dir[:-1]
+
+    while stage_0_dir[-1] == '/':
+        stage_0_dir = stage_0_dir[:-1]
+
+
+    build_dir = build_dir + '/'
+    install_dir = install_dir + '/'
+    matchable_dir = matchable_dir + '/'
+
+    shutil.rmtree(build_dir, ignore_errors=True)
+    os.makedirs(build_dir)
+    os.chdir(build_dir)
+
+    shutil.rmtree(install_dir, ignore_errors=True)
+    os.makedirs(install_dir)
+
+    cmake_cmd = ['cmake', '-DCMAKE_INSTALL_PREFIX=' + install_dir]
+    cmake_cmd.append('-Dmatchable_DIR=' + matchable_dir + '/lib/matchable/cmake')
+    cmake_cmd.append('-Dmatchmaker_DIR=' + stage_0_dir + '/lib/matchmaker/cmake')
+
+    if use_clang:
+        clang_c = which('clang')
+        clang_cxx = which('clang++')
+        if clang_c is None or clang_cxx is None:
+            print('search for clang compiler failed')
+            exit(1)
+        cmake_cmd.append('-DCMAKE_C_COMPILER=' + clang_c)
+        cmake_cmd.append('-DCMAKE_CXX_COMPILER=' + clang_cxx)
+
+    if debug:
+        cmake_cmd.append('-DCMAKE_BUILD_TYPE=Debug')
+    else:
+        cmake_cmd.append('-DCMAKE_BUILD_TYPE=Release')
+
+    cmake_cmd.append(data_reader_stage_1_root)
+
+    if subprocess.run(cmake_cmd).returncode != 0:
+        print('cmake failed')
+        os.chdir(data_reader_stage_1_root)
+        exit(1)
+
+    if subprocess.run(['make', '-j' + str(multiprocessing.cpu_count()), 'install']).returncode != 0:
+        print('make failed')
+        os.chdir(data_reader_stage_1_root)
+        exit(1)
+
+    os.chdir(start_dir)
+
+
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'hcb:i:m:s:d',
+                ['help', 'clang', 'build_dir', 'install_dir', 'matchable_dir', 'stage_0_dir' 'debug'])
+    except getopt.GetoptError as err:
+        print(err)
+        usage()
+        sys.exit(2)
+
+    use_clang = False
+    build_dir = ''
+    install_dir = ''
+    matchable_dir = ''
+    stage_0_dir = ''
+    debug = False
+
+    for o, a in opts:
+        if o in ('-h', '--help'):
+            usage()
+            sys.exit()
+        elif o in ('-c', '--clang'):
+            use_clang = True
+        elif o in ('-b', '--build_dir'):
+            build_dir = a
+        elif o in ('-i', '--install_dir'):
+            install_dir = a
+        elif o in ('-m', '--matchable_dir'):
+            matchable_dir = a
+        elif o in ('-s', '--stage_0_dir'):
+            stage_0_dir = a
+        elif o in ('-d', '--debug'):
+            debug = True
+        else:
+            assert False, "unhandled option"
+
+    build_and_install(build_dir, install_dir, matchable_dir, stage_0_dir, use_clang, debug)
+
+    exit(0)
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ data_reader/stage_1/src/data_reader_stage_1.cpp
@@ -0,0 +1,404 @@
+/*
+Copyright (c) 2020, Eric Hyer
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+
+#include <iostream>
+#include <filesystem>
+#include <regex>
+
+#include <matchable/matchable.h>
+#include <matchable/matchable_fwd.h>
+
+#include <matchable/MatchableMaker.h>
+#include <matchmaker/matchmaker.h>
+
+
+MATCHABLE(
+    word_status,
+    not_printable_ascii,
+    has_spaces,
+    has_hyphens,
+    has_other_symbols
+)
+
+
+struct entry_51155
+{
+    std::vector<std::string> syn;
+    std::vector<std::string> ant;
+};
+
+
+
+void print_usage();
+
+void read_51155(
+    FILE * input_file,
+    std::map<std::string, entry_51155> & contents_51155
+);
+
+void update_word_status(word_status::Flags & flags, int & ch);
+
+bool create_stage_1_prefix(
+    std::filesystem::directory_entry const & stage_0_header_entry,
+    std::map<std::string, entry_51155> const & contents_51155
+);
+
+
+int main(int argc, char ** argv)
+{
+    if (argc != 3)
+    {
+        print_usage();
+        return 2;
+    }
+
+    std::string const DATA_DIR{argv[1]};
+    std::string const STAGE_0_MATCHABLES_DIR{argv[2]};
+
+    std::cout << "creating matchables for stage 1:\n" << std::endl;
+    std::cout << "       ------> reading 51155 ..............: " << std::flush;
+
+    std::map<std::string, entry_51155> contents_51155;
+
+    {
+        std::string const FN_51155{DATA_DIR + "/51155/51155-0.txt"};
+        FILE * f = fopen(FN_51155.c_str(), "r");
+        if (f == 0)
+        {
+            perror(FN_51155.c_str());
+            return 1;
+        }
+        read_51155(f, contents_51155);
+        fclose(f);
+    }
+
+    std::cout << "done" << std::endl;
+
+    for (auto const & entry : std::filesystem::recursive_directory_iterator(STAGE_0_MATCHABLES_DIR))
+        if (entry.is_regular_file())
+            if (!create_stage_1_prefix(entry, contents_51155))
+                return 1;
+
+    std::cout << "\nstage 1 matchables ready!\n" << std::endl;
+
+    return 0;
+}
+
+
+
+void print_usage()
+{
+    std::cout << "program expects 2 arguments:\n"
+              << "    [1]  data directory\n"
+              << "    [2]  stage 0 matchables directory\n"
+              << std::flush;
+}
+
+
+void read_51155(FILE * input_file, std::map<std::string, entry_51155> & contents_51155)
+{
+    std::string word;
+    std::string cur_key{"-1"};
+    word_status::Flags status;
+    bool key{false};
+    bool syn{false};
+    bool ant{false};
+    int ch{0};
+
+    while (true)
+    {
+        key = false;
+        syn = false;
+        ant = false;
+
+        ch = fgetc(input_file);
+        if (ch == 'K')
+        {
+            ch = fgetc(input_file);
+            if (ch == 'E')
+            {
+                ch = fgetc(input_file);
+                if (ch == 'Y')
+                {
+                    ch = fgetc(input_file);
+                    if (ch == ':')
+                    {
+                        ch = fgetc(input_file);
+                        if (ch == ' ')
+                            key = true;
+                    }
+                }
+            }
+        }
+        else if (cur_key != "-1" && ch == 'S')
+        {
+            ch = fgetc(input_file);
+            if (ch == 'Y')
+            {
+                ch = fgetc(input_file);
+                if (ch == 'N')
+                {
+                    ch = fgetc(input_file);
+                    if (ch == ':')
+                    {
+                        ch = fgetc(input_file);
+                        if (ch == ' ')
+                            syn = true;
+                    }
+                }
+            }
+        }
+        else if (cur_key != "-1" && ch == 'A')
+        {
+            ch = fgetc(input_file);
+            if (ch == 'N')
+            {
+                ch = fgetc(input_file);
+                if (ch == 'T')
+                {
+                    ch = fgetc(input_file);
+                    if (ch == ':')
+                    {
+                        ch = fgetc(input_file);
+                        if (ch == ' ')
+                            ant = true;
+                    }
+                }
+            }
+        }
+
+        if (key)
+        {
+            word.clear();
+            status.clear();
+            while (true)
+            {
+                ch = fgetc(input_file);
+
+                if (ch == EOF)
+                    return;
+
+                if (ch == ' ' || ch == '_' || ch == '.' || ch == 10 || ch == 13)
+                    break;
+
+                if (ch >= 'A' && ch <= 'Z')
+                    ch += 32;
+
+                update_word_status(status, ch);
+                word += (char) ch;
+            }
+            if (word.size() > 0 && !status.is_set(word_status::has_other_symbols::grab()))
+                cur_key = word;
+            else
+                cur_key = "-1";
+        }
+        else if (syn || ant)
+        {
+            while (ch != '.')
+            {
+                word.clear();
+                status.clear();
+
+                // read word
+                while (true)
+                {
+                    ch = fgetc(input_file);
+
+                    if (ch == EOF)
+                        return;
+
+                    if (ch == ' ' || ch == '.' || ch == ',' || ch == 10 || ch == 13)
+                        break;
+
+                    if (ch >= 'A' && ch <= 'Z')
+                        ch += 32;
+
+                    update_word_status(status, ch);
+                    word += (char) ch;
+                }
+
+                // maybe add word
+                if (word.size() > 0)
+                {
+                    if (status.is_set(word_status::not_printable_ascii::grab()))
+                        continue;
+
+                    if (status.is_set(word_status::has_spaces::grab()))
+                        continue;
+
+                    if (status.is_set(word_status::has_hyphens::grab()))
+                        continue;
+
+                    if (status.is_set(word_status::has_other_symbols::grab()))
+                        continue;
+
+                    if (syn)
+                        contents_51155[cur_key].syn.push_back(word);
+
+                    if (ant)
+                        contents_51155[cur_key].ant.push_back(word);
+                }
+            }
+        }
+
+        // skip to end of line
+        while (ch != 10 && ch != 13)
+        {
+            ch = fgetc(input_file);
+            if (ch == EOF)
+                return;
+        }
+
+        ungetc(ch, input_file);
+
+        // skip to beginning of next line
+        while (ch == 10 || ch == 13)
+        {
+            ch = fgetc(input_file);
+            if (ch == EOF)
+                return;
+
+            if (ch != 10 && ch != 13)
+                ungetc(ch, input_file);
+        }
+    }
+}
+
+
+
+void update_word_status(word_status::Flags & flags, int & ch)
+{
+    if (ch < 32 || ch > 126)
+    {
+        flags.set(word_status::not_printable_ascii::grab());
+        ch = '?';
+    }
+    else if (ch < 'A' || (ch > 'Z' && ch < 'a') || ch > 'z')
+    {
+        if (ch == ' ')
+            flags.set(word_status::has_spaces::grab());
+        else if (ch == '-')
+            flags.set(word_status::has_hyphens::grab());
+        else
+            flags.set(word_status::has_other_symbols::grab());
+    }
+}
+
+
+
+bool create_stage_1_prefix(
+    std::filesystem::directory_entry const & stage_0_header_entry,
+    std::map<std::string, entry_51155> const & contents_51155
+)
+{
+    matchable::MatchableMaker mm;
+    matchable::load__status::Type load_status = mm.load(stage_0_header_entry.path());
+    if (load_status != matchable::load__status::success::grab())
+    {
+        std::cout << "attempt to load " << stage_0_header_entry << " failed with: "
+                  << load_status << std::endl;
+        return false;
+    }
+
+    std::string existing_word;
+    bool found{false};
+    int index{-1};
+
+    for (auto const & [str, m] : mm.matchables)
+    {
+        if (str.rfind("word", 0) != std::string::npos)
+        {
+            std::string prefix = str.substr(5);
+            prefix = std::regex_replace(
+                prefix,
+                std::regex("_"),
+                " "
+            );
+            std::cout << "       ------> patching " << prefix;
+            for (int i = prefix.size(); i < 5; ++i)
+                std::cout << ".";
+            std::cout << "..............: " << std::flush;
+
+            std::vector<std::string> syn_vect;
+            std::vector<std::string> ant_vect;
+            for (auto const & v : m->variants)
+            {
+                syn_vect.clear();
+                ant_vect.clear();
+
+                existing_word = matchable::escapable::unescape_all(v.variant_name);
+
+                auto contents_51155_iter = contents_51155.find(existing_word);
+                if (contents_51155_iter != contents_51155.end())
+                {
+                    for (auto const & s : contents_51155_iter->second.syn)
+                    {
+                        index = matchmaker::lookup(s, &found);
+                        if (found)
+                            syn_vect.push_back(std::to_string(index));
+                    }
+                    for (auto const & a : contents_51155_iter->second.ant)
+                    {
+                        index = matchmaker::lookup(a, &found);
+                        if (found)
+                            ant_vect.push_back(std::to_string(index));
+                    }
+                }
+
+                m->set_propertyvect(v.variant_name, "syn", syn_vect);
+                m->set_propertyvect(v.variant_name, "ant", ant_vect);
+            }
+
+            break;
+        }
+    }
+
+    std::string output_fn = stage_0_header_entry.path();
+    output_fn = std::regex_replace(
+        output_fn,
+        std::regex("generated_matchables_stage_0"),
+        "generated_matchables_stage_1"
+    );
+    std::filesystem::path output_path{output_fn};
+    std::filesystem::create_directories(output_path.parent_path());
+
+    auto sa_status = mm.save_as(
+        output_fn,
+        {matchable::save_as__content::matchables::grab()},
+        matchable::save_as__spread_mode::wrap::grab()
+    );
+
+    std::cout << sa_status << std::endl;
+
+    if (sa_status != matchable::save_as__status::success::grab())
+        return false;
+    return true;
+}
